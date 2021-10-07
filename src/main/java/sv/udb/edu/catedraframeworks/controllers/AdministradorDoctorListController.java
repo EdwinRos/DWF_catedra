a8d22f99861b27e44cf64479fab108d2f048a48b
FernandoMerlos_25 <79458666+8888220@users.noreply.github.com>
2021-10-06 23:53:44 -0300

    Ultimo ya
    F

--- src/main/java/sv/udb/edu/catedraframeworks/controllers/AdministradorDoctorListController.java
+++ src/main/java/sv/udb/edu/catedraframeworks/controllers/AdministradorDoctorListController.java
@@ -18,7 +18,7 @@ import sv.udb.edu.catedraframeworks.repositories.DoctorRepository;
 @Scope(value = "session")
 @Component(value = "doctorList")
 @ELBeanName(value = "doctorList")
-@Join(path = "/listadoDoctor", to = "/administrador/administrador-listado-doctores.jsf")
+@Join(path = "/listadodoctor", to = "/administrador/administrador-listado-doctores.jsf")
 public class AdministradorDoctorListController {
 	
 	 @Autowired
