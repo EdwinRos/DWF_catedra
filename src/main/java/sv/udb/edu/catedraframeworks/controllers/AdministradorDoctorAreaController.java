8fd080576f08023c3f5fcb9e0d974b7a40f914a2
FernandoMerlos_25 <79458666+8888220@users.noreply.github.com>
2021-10-07 16:13:59 -0300

    Vistas Administrador
    Ya estan en funcionamiento las vistas, queda pendiente las vistas AreaDoctor

---
+++ src/main/java/sv/udb/edu/catedraframeworks/controllers/AdministradorDoctorAreaController.java
@@ -0,0 +1,21 @@
+package sv.udb.edu.catedraframeworks.controllers;
+
+import java.util.List;
+
+import org.ocpsoft.rewrite.annotation.Join;
+import org.ocpsoft.rewrite.el.ELBeanName;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Scope(value = "session")
+@Component(value = "AdminAreaDoctorControler")
+@ELBeanName(value = "AdminAreaDoctorControler")
+@Join(path = "/admindoctorarea", to = "/administrador/administrador-doctor-area-nuevo.jsf")
+public class AdministradorDoctorAreaController {
+
+
+
+	
+	
+	
+}
