ba02cc2842aa892a43cd3e03c51e0730ce3a8e7a
Victor5032 <victorlopez_20@outlook.com>
2021-10-07 13:15:29 -0600

    Vista de detalles de la cita

--- src/main/java/sv/udb/edu/catedraframeworks/controllers/CitaDetallesController.java
+++ src/main/java/sv/udb/edu/catedraframeworks/controllers/CitaDetallesController.java
@@ -1,18 +1,53 @@
 package sv.udb.edu.catedraframeworks.controllers;
 
+import java.util.Optional;
+
 import org.ocpsoft.rewrite.annotation.Join;
+import org.ocpsoft.rewrite.annotation.RequestAction;
 import org.ocpsoft.rewrite.el.ELBeanName;
+import org.ocpsoft.rewrite.faces.annotation.Deferred;
+import org.ocpsoft.rewrite.faces.annotation.IgnorePostback;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-import sv.udb.edu.catedraframeworks.entities.Doctor;
-import sv.udb.edu.catedraframeworks.repositories.DoctorRepository;
+import sv.udb.edu.catedraframeworks.entities.Citas;
+import sv.udb.edu.catedraframeworks.repositories.CitasRepository;
+import sv.udb.edu.catedraframeworks.utils.ActualAge;
+import sv.udb.edu.catedraframeworks.utils.JsfUtil;
 
 @Scope(value = "session")
 @Component(value = "citaDetallesController")
 @ELBeanName(value = "citaDetallesController")
 @Join(path = "/cita/detalles", to="/doctor/detalles-cita.jsf")
-
 public class CitaDetallesController {
+	@Autowired
+    private CitasRepository citaRepository;
+    Citas cita = new Citas();
+    int edad = 0;
+    
+    @Deferred
+    @RequestAction
+    @IgnorePostback
+    public void loadData() {
+    	ActualAge edadPaciente = new ActualAge();
+    	int idCita = Integer.valueOf(JsfUtil.getRequest().getParameter("citaId"));
+    	
+    	Optional<Citas> miCita = citaRepository.findById(idCita);
+    	
+    	cita.setCitasId(miCita.get().getCitasId());
+    	cita.setExtras(miCita.get().getExtras());
+    	cita.setFechaCita(miCita.get().getFechaCita());
+    	cita.setHoraCita(miCita.get().getHoraCita());
+    	cita.setIdPaciente(miCita.get().getIdPaciente());
+    	
+    	edad = edadPaciente.getActualDate(miCita.get().getIdPaciente().getFechaNacimiento());
+    }
+    
+    public Citas getCita() {
+        return cita;
+    }
 
+    public int getEdad() {
+        return edad;
+    }
 }
