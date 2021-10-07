1d6f6efb181aa42356c7f30033dba43d6f7a9094
EdwinRos <e.edwirosales@gmail.com>
2021-10-06 18:01:23 -0600

    Clase para generar contraseña aleatoria: RamdomString.java devuelve String ramdom en el metodo password

---
+++ src/main/java/sv/udb/edu/catedraframeworks/utils/RamdomString.java
@@ -0,0 +1,21 @@
+package sv.udb.edu.catedraframeworks.utils;
+
+import java.util.Random;
+
+public class RamdomString {
+
+    public String password() {
+        String password = "";
+        Random r = new Random();
+
+        String alphabet = "abcdefghijklmnopqrstuvwxyz"
+                + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        for (int i = 0; i < 8; i++) {
+            password += (alphabet.charAt(r.nextInt(alphabet.length())));
+        }
+
+        return password;
+    }
+
+}
