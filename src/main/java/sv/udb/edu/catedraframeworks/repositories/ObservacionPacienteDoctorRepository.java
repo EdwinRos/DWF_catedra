d3f315caa761d7cdb875c16e7cdf3aa05988e500
Edwin Rosales <e.orellana04@outlook.com>
2021-10-08 12:48:17 -0600

    Enviar observaciones a un doctor, poder observar las valoraciones de un paciente a un doctor y el rating del mismo

---
+++ src/main/java/sv/udb/edu/catedraframeworks/repositories/ObservacionPacienteDoctorRepository.java
@@ -0,0 +1,13 @@
+package sv.udb.edu.catedraframeworks.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sv.udb.edu.catedraframeworks.entities.Doctor;
+import sv.udb.edu.catedraframeworks.entities.ObservacionPacienteDoctor;
+
+import java.util.List;
+
+public interface ObservacionPacienteDoctorRepository extends JpaRepository<ObservacionPacienteDoctor, Integer> {
+
+    List<ObservacionPacienteDoctor> getObservacionPacienteDoctorByIdDoctor(Doctor id);
+
+}
