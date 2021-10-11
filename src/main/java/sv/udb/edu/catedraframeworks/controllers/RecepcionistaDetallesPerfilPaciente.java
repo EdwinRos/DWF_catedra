6bea5c988a43b142a994100d38800fc9f3269e2f
Emerson21983 <55989868+Emerson21983@users.noreply.github.com>
2021-10-10 23:13:24 -0600

    Consultar perfil de paciente por dui terminado

--- src/main/java/sv/udb/edu/catedraframeworks/controllers/RecepcionistaDetallesPerfilPaciente.java
+++ src/main/java/sv/udb/edu/catedraframeworks/controllers/RecepcionistaDetallesPerfilPaciente.java
@@ -1,5 +1,6 @@
 package sv.udb.edu.catedraframeworks.controllers;
 
+
 import org.ocpsoft.rewrite.annotation.Join;
 import org.ocpsoft.rewrite.annotation.RequestAction;
 import org.ocpsoft.rewrite.el.ELBeanName;
@@ -8,34 +9,30 @@ import org.ocpsoft.rewrite.faces.annotation.IgnorePostback;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-import sv.udb.edu.catedraframeworks.entities.Area;
 import sv.udb.edu.catedraframeworks.entities.Citas;
-import sv.udb.edu.catedraframeworks.entities.Doctor;
+import sv.udb.edu.catedraframeworks.entities.Paciente;
 import sv.udb.edu.catedraframeworks.repositories.CitasRepository;
+import sv.udb.edu.catedraframeworks.repositories.PacienteRepository;
 import sv.udb.edu.catedraframeworks.utils.JsfUtil;
 
-import java.util.List;
-import java.util.Optional;
-
 @Scope(value = "session")
-@Component(value = "RecepcionistaDetallesCitaController")
-@ELBeanName(value = "RecepcionistaDetallesCitaController")
-@Join(path = "/detallescitapaciente", to="/recepcionista/detalles-cita-paciente.jsf")
-public class RecepcionistaDetallesCitaPaciente {
+@Component(value = "RecepcionistaDetallesPerfilPaciente")
+@ELBeanName(value = "RecepcionistaDetallesPerfilPaciente")
+@Join(path = "/detallesperfilpaciente", to="/recepcionista/detalles-perfil-paciente.jsf")
+public class RecepcionistaDetallesPerfilPaciente {
     @Autowired
-    private CitasRepository citaRepository;
-    Citas cita = new Citas();
+    private PacienteRepository pacienteRepository;
+    Paciente paciente = new Paciente();
 
     @Deferred
     @RequestAction
     @IgnorePostback
-    public void cargarDetallesCita() {
-        String codigoCita = JsfUtil.getRequest().getParameter("codigo");
-
-        cita = citaRepository.findByCodigoCita(codigoCita);
+    public void cargarDetallesPaciente() {
+        String duiPaciente = JsfUtil.getRequest().getParameter("dui");
+        paciente = pacienteRepository.findByDuiPaciente(duiPaciente);
     }
 
-    public Citas getCita() {
-        return cita;
+    public Paciente getPaciente() {
+        return paciente;
     }
 }
--- src/main/java/sv/udb/edu/catedraframeworks/repositories/PacienteRepository.java
+++ src/main/java/sv/udb/edu/catedraframeworks/repositories/PacienteRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import sv.udb.edu.catedraframeworks.entities.Paciente;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Integer> {
+    Paciente findByDuiPaciente(String dui);
 }
