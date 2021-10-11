0056a5161997be2aa5f674a03efa58607c482ecd
Victor5032 <victorlopez_20@outlook.com>
2021-10-10 22:20:38 -0600

    Seccion de comentarios en el área de doctor, ya supe como hacer lo de ordenar en los repositorios UwU y Solo me queda pendiente agendar cita junto con el Login

---
+++ src/main/java/sv/udb/edu/catedraframeworks/controllers/FeedbacksController.java
@@ -0,0 +1,71 @@
+package sv.udb.edu.catedraframeworks.controllers;
+
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import org.ocpsoft.rewrite.annotation.Join;
+import org.ocpsoft.rewrite.annotation.RequestAction;
+import org.ocpsoft.rewrite.el.ELBeanName;
+import org.ocpsoft.rewrite.faces.annotation.Deferred;
+import org.ocpsoft.rewrite.faces.annotation.IgnorePostback;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import sv.udb.edu.catedraframeworks.entities.Doctor;
+import sv.udb.edu.catedraframeworks.repositories.DoctorRepository;
+import sv.udb.edu.catedraframeworks.entities.ObservacionesDoctor;
+import sv.udb.edu.catedraframeworks.repositories.ObservacionesDoctorRepository;
+import sv.udb.edu.catedraframeworks.entities.ObservacionPacienteDoctor;
+import sv.udb.edu.catedraframeworks.repositories.ObservacionPacienteDoctorRepository;
+import sv.udb.edu.catedraframeworks.utils.JsfUtil;
+
+@Scope(value = "session")
+@Component(value = "feedbackController")
+@ELBeanName(value = "feedbackController")
+@Join(path = "/doctor/comentarios", to = "/doctor/comentarios.jsf")
+public class FeedbacksController {
+	@Autowired
+	private ObservacionesDoctorRepository observacionesDoctorRepository;
+	private List<ObservacionesDoctor> observacionesDoctor;
+	
+	@Autowired
+	private DoctorRepository doctorRepository;
+	Doctor doctor = new Doctor();
+	
+	@Autowired
+	private ObservacionPacienteDoctorRepository observacionesPacientesRepository;
+	private List<ObservacionPacienteDoctor> observacionesPacientes;
+	
+	@Deferred
+    @RequestAction
+    @IgnorePostback
+    public void loadData() {
+		int idDoctor = 1;
+		
+		Doctor doctor = doctorRepository.getById(idDoctor);
+		
+		observacionesSupervisor(doctor);
+		observacionesPaciente(doctor);
+	}
+	
+	protected void observacionesSupervisor(Doctor doctor) {
+		observacionesDoctor = observacionesDoctorRepository.findObservacionesDoctorByIdDoctorOrderByFechaRegistroDesc(doctor);
+	}
+	
+	protected void observacionesPaciente(Doctor doctor) {
+		observacionesPacientes = observacionesPacientesRepository.findObservacionPacienteDoctorByIdDoctorOrderByFechaRegistroDesc(doctor);
+	}
+	
+	// Getters
+	public List<ObservacionesDoctor> getObservacionesDoctor() {
+		return observacionesDoctor;
+	}
+	
+	public List<ObservacionPacienteDoctor> getObservacionesPacientes() {
+		return observacionesPacientes;
+	}
+}
+
