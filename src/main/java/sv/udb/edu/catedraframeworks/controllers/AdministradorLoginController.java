1c1858f58e2e869cb41039948c92c34d55a1188d
FernandoMerlos_25 <79458666+8888220@users.noreply.github.com>
2021-10-11 22:01:35 -0300

    Login y vistas(Administrador)
    Ahi te voy

---
+++ src/main/java/sv/udb/edu/catedraframeworks/controllers/AdministradorLoginController.java
@@ -0,0 +1,65 @@
+package sv.udb.edu.catedraframeworks.controllers;
+
+import java.security.NoSuchAlgorithmException;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+import org.ocpsoft.rewrite.annotation.Join;
+import org.ocpsoft.rewrite.el.ELBeanName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import sv.udb.edu.catedraframeworks.entities.Administrador;
+import sv.udb.edu.catedraframeworks.repositories.AdministradorRepository;
+import sv.udb.edu.catedraframeworks.utils.HashSha1;
+
+@Scope(value = "session")
+@Component(value = "AdminLogin")
+@ELBeanName(value = "AdminLogin")
+@Join(path = "/administrador/login", to = "/administrador/admin-login.jsf")
+public class AdministradorLoginController {
+
+	
+
+	FacesContext facesContext = FacesContext.getCurrentInstance();
+    HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(true);
+	
+    
+    @Autowired
+  private AdministradorRepository adminstradoRepository;
+    Administrador administrador = new Administrador();
+   String passwordAdmin;
+    
+    public String IniciarSesionDoctor() throws NoSuchAlgorithmException{
+    	HashSha1 sha1 = new HashSha1();
+    	administrador = adminstradoRepository.findByPasswordAndUsuario(administrador.getUsuario(), sha1.hashPassword(administrador.getPassword()));
+   	 if (administrador != null) {
+   		 session.setAttribute("id", administrador.getAdministradorId());
+   		 session.setAttribute("usuario",administrador.getUsuario());
+   		 passwordAdmin = null;
+   		 
+   		session.setAttribute("id", administrador.getAdministradorId());
+   		
+   		return "/administrador/ingresardoctor.xhtml?faces-redirect=true";
+			
+		}else {
+			administrador = null;
+			
+			return "/administrador/ingresardoctor.xhtml?faces-redirect=true";
+		}
+    	
+    }
+    
+    public String logOut() {
+    	session.invalidate();
+    	 return "/administrador/admin-login.xhtml?faces-redirect=true";
+    }
+    
+    public Administrador getAdministrador() {
+    	return administrador;
+	}
+	
+}
